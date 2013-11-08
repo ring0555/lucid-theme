abfcb0e051b9607b5ad0784464835bbffb08d69b
Nathan Hefner <hefner.nathan@gmail.com>
2013-11-07 19:08:25 -0500

    Page default is full width, removed extra full width custom page

--- page.php
+++ page.php
@@ -2,29 +2,15 @@
 
 <div class="container">
 
-  <div class="row">
+  <div class="page">
 
-    <div class="col-lg-9">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-      <div class="page">
+      <h1 class="page-title"><?php the_title(); ?></h1>
 
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php the_content(); ?>
 
-        <h1 class="page-title"><?php the_title(); ?></h1>
-
-        <?php the_content(); ?>
-
-      <?php endwhile; endif; ?>
-
-      </div>
-
-    </div>
-
-    <div class="col-lg-3">
-
-      <?php get_sidebar(); ?>
-
-    </div>
+    <?php endwhile; endif; ?>
 
   </div>
 
