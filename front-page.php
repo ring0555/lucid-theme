d080e25b603f1a1a71e16f8ef194471c1152aa03
Nathan Hefner <hefner.nathan@gmail.com>
2013-11-08 23:27:55 -0500

    Minor front page style fix

--- front-page.php
+++ front-page.php
@@ -13,15 +13,15 @@
   <div class="process">
     <h2 class="heading">The Process</h2>
     <div class="row">
-      <div class="col-sm-4">
+      <div class="col-xs-4">
         <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/bedrock/img/design-icon.png">
         <h3>Design</h3>
       </div>
-      <div class="col-sm-4">
+      <div class="col-xs-4">
         <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/bedrock/img/code-icon.png">
         <h3>Develop</h3>
       </div>
-      <div class="col-sm-4">
+      <div class="col-xs-4">
         <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/bedrock/img/server-icon.png">
         <h3>Deploy</h3>
       </div>
@@ -33,24 +33,24 @@
   <div class="services">
     <h2 class="heading">Our Services</h2>
     <div class="row">
-      <div class="col-lg-4">
+      <div class="col-sm-4">
         <h3>Web Design</h3>
       </div>
-      <div class="col-lg-4">
+      <div class="col-sm-4">
         <h3>Web Development</h3>
       </div>
-      <div class="col-lg-4">
+      <div class="col-sm-4">
         <h3>Android Development</h3>
       </div>
     </div>
     <div class="row">
-      <div class="col-lg-4">
+      <div class="col-sm-4">
         <h3>Scalability and Performance</h3>
       </div>
-      <div class="col-lg-4">
+      <div class="col-sm-4">
         <h3>Code Review</h3>
       </div>
-      <div class="col-lg-4">
+      <div class="col-sm-4">
         <h3>Software Packages</h3>
       </div>
     </div>
