92b1a7b1790d6bd0423d862be4a32600b7ab59a7
Nathan Hefner <hefner.nathan@gmail.com>
2013-11-08 21:21:29 -0500

    Finished front page

--- front-page.php
+++ front-page.php
@@ -1,8 +1,67 @@
 <?php get_header(); ?>
 
+<div class="jumbotron">
+
+  <h2>Developing</h2>
+  <h1>High End Applications</h1>
+  <h2>For the Web and Mobile</h2>
+
+</div>
+
 <div class="container">
 
-  <h1>Test</h1>
+  <div class="process">
+    <h2 class="heading">The Process</h2>
+    <div class="row">
+      <div class="col-sm-4">
+        <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/bedrock/img/design-icon.png">
+        <h3>Design</h3>
+      </div>
+      <div class="col-sm-4">
+        <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/bedrock/img/code-icon.png">
+        <h3>Develop</h3>
+      </div>
+      <div class="col-sm-4">
+        <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/bedrock/img/server-icon.png">
+        <h3>Deploy</h3>
+      </div>
+    </div>
+  </div>
+
+  <hr class="divider"/>
+
+  <div class="services">
+    <h2 class="heading">Our Services</h2>
+    <div class="row">
+      <div class="col-lg-4">
+        <h3>Web Design</h3>
+      </div>
+      <div class="col-lg-4">
+        <h3>Web Development</h3>
+      </div>
+      <div class="col-lg-4">
+        <h3>Android Development</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <h3>Scalability and Performance</h3>
+      </div>
+      <div class="col-lg-4">
+        <h3>Code Review</h3>
+      </div>
+      <div class="col-lg-4">
+        <h3>Software Packages</h3>
+      </div>
+    </div>
+  </div>
+
+  <hr class="divider"/>
+
+  <div class="letswork">
+    <h2 class="heading">Let's Work</h2>
+    <a href="<?php bloginfo('url'); ?>/contact/" class="btn btn-default">Contact Us</a>
+  </div>
 
 </div>
 
--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
 
+function get_ID_by_slug($page_slug) {
+  $page = get_page_by_path($page_slug);
+  if ($page) {
+    return $page->ID;
+  } else {
+    return null;
+  }
+}
+
 ?>
