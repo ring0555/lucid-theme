3cb7818b134fa337e4c2f8d027d8df469cf03ef8
Nathan Hefner <hefner.nathan@gmail.com>
2013-11-11 16:31:42 -0500

    Added work page, added support for featured images

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+/******************************/
+/* VARIOUS UTILITY FUNCTIONS **/
+/******************************/
+
 function get_ID_by_slug($page_slug) {
   $page = get_page_by_path($page_slug);
   if ($page) {
@@ -8,5 +12,62 @@ function get_ID_by_slug($page_slug) {
     return null;
   }
 }
+
+/******************************/
+/**** CUSTOM WORK URL META ****/
+/******************************/
+
+function add_work_url_box() {
+  add_meta_box(
+    'work_url',
+    'URL',
+    'show_work_url_box',
+    'post',
+    'normal',
+    'high');
+}
+add_action('add_meta_boxes', 'add_work_url_box');
+
+function show_work_url_box() {
+  global $post;
+    $meta = get_post_meta($post->ID, 'work_url', true);
+
+  echo '<input type="hidden" name="custom_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'" />';
+
+  echo '<table class="form-table">';
+  echo '<tr>
+        <th><label for="work_url">Work URL</label></th>
+        <td><input type="text" name="work_url" id="work_url" value="'.$meta.'">
+        </tr>';
+  echo '</table>';
+}
+
+function save_work_url_meta($post_id) {
+  if (!wp_verify_nonce($_POST['custom_meta_box_nonce'], basename(__FILE__)))
+      return $post_id;
+
+  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+      return $post_id;
+
+  if ('page' == $_POST['post_type']) {
+      if (!current_user_can('edit_page', $post_id))
+          return $post_id;
+  } elseif (!current_user_canw('edit_post', $post_id)) {
+      return $post_id;
+  }
+
+  $old = get_post_meta($post_id, "work_url", true);
+  $new = $_POST["work_url"];
+
+  if ($new && $new != $old) {
+      update_post_meta($post_id, "work_url", $new);
+  } elseif ('' == $new && $old) {
+      delete_post_meta($post_id, "work_url", $old);
+  }
+}
+
+add_action('save_post', 'save_work_url_meta');
+
+add_theme_support('post-thumbnails');
 
 ?>
---
+++ work-page.php
@@ -0,0 +1,51 @@
+<?php
+/*
+* Template Name: Work Page
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+  <div class="row">
+
+    <div class="col-lg-10 col-lg-offset-1">
+
+      <?php if (have_posts()) : ?>
+        <?php query_posts('category_name=Work'); ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php $url = get_post_meta($post->ID, "work_url", true); ?>
+
+        <div class="row work">
+
+          <div class="col-sm-6">
+
+            <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail') ); ?>
+            <img src="<?php echo $url; ?>">
+
+          </div>
+
+          <div class="col-sm-6">
+
+            <h1 class="title"><a href="http://<?php echo $url; ?>"><?php the_title(); ?></a></h1>
+
+            <div class="summary"><?php the_content(); ?></div>
+
+            <a class="btn btn-default btn-sm url" href="http://<?php echo $url; ?>" target="_blank">View Project</a>
+
+          </div>
+
+        </div>
+
+        <?php endwhile; ?>
+      <?php endif; ?>
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
