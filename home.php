a28a99e2c0052d8d9bfd2832136775fa1a438f86
Nathan Hefner <hefner.nathan@gmail.com>
2014-04-23 10:47:03 -0400

    Updated post styling

--- home.php
+++ home.php
@@ -1,29 +1,24 @@
 <?php get_header(); ?>
 
 <div class="container">
-
   <div class="posts">
 
   <?php if (have_posts()) : ?>
-  <?php query_posts('category_name=Journal'); ?>
   <?php while ( have_posts() ) : the_post(); ?>
 
     <div class="post">
-
-      <h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-
-      <h5><span class="date"><?php the_time('l, F jS, Y'); ?></span>&loz;<span class="author">
-        By: <?php the_author(); ?></h5>
-
+      <div class="top">
+        <h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <h5 class="date"><?php the_time('F jS, Y'); ?></h5>
+      </div>
+      <h5 class="author">By: <?php the_author(); ?></h5>
       <div class="summary"><?php the_content('Read More'); ?></div>
-
     </div>
 
   <?php endwhile; ?>
   <?php endif; ?>
 
   </div>
-
 </div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 
 <div class="container">
-
-  <div class="single-post post">
+  <div class="single-post">
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-      <h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-
-      <h5><span class="date"><?php the_time('l, F jS, Y'); ?></span>&loz;<span class="author">
-        By: <?php the_author(); ?></h5>
-
+    <div class="post">
+      <div class="top">
+        <h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <h5 class="date"><?php the_time('F jS, Y'); ?></h5>
+      </div>
+      <h5 class="author">By: <?php the_author(); ?></h5>
       <div class="post-content"><?php the_content(); ?></div>
 
     <?php endwhile; else: ?>
