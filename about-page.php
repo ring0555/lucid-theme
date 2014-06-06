06754e98a0b4c2955396e1e095860be55e840815
Nathan Hefner <hefner.nathan@gmail.com>
2014-06-06 14:51:24 -0400

    Adding some files I will need later

---
+++ about-page.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: About Page
+*/
+?>
+
+<?php get_header(); ?>
+<div class="container">
+  <div class="about">
+    <h1>About Us</h1>
+  </div>
+</div>
+<?php get_footer(); ?>
