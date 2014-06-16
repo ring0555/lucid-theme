84af0c6eec99c249961d7680a53601516b438add
Nathan Hefner <hefner.nathan@gmail.com>
2014-06-16 15:33:59 -0400

    New header and footer

--- footer.php
+++ footer.php
@@ -1,33 +1,33 @@
-  <div class="footer">
-    <div class="container">
-      <hr>
-      <div class="row">
-        <div class="col-sm-4">
-          <a href="mailto:hello@lucidstudios.co" target="_blank">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/img/email.png">
-            hello@lucidstudios.co
-          </a>
-        </div>
-        <div class="col-sm-4">
-          <a href="http://twitter.com/LucidStudiosCo" target="_blank">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/img/twitter.png">
-            LucidStudiosCo
-          </a>
-        </div>
-        <div class="col-sm-4">
-          <a href="http://github.com/LucidStudiosCo" target="_blank">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/img/github.png">
-            LucidStudiosCo
-          </a>
-        </div>
-      </div>
-      <div class="copyright">
-        <p>&copy; 2013 - Lucid Studios, LLC</p>
-      </div>
-    </div>
-  </div>
+          <div class="footer hidden-xl hidden-lg">
+            <div class="container">
+              <hr>
+              <div class="row">
+                <div class="col-sm-4">
+                  <a href="mailto:hello@lucidstudios.co" target="_blank">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/email.png">
+                    hello@lucidstudios.co
+                  </a>
+                </div>
+                <div class="col-sm-4">
+                  <a href="http://twitter.com/LucidStudiosCo" target="_blank">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/twitter.png">
+                    LucidStudiosCo
+                  </a>
+                </div>
+                <div class="col-sm-4">
+                  <a href="http://github.com/LucidStudiosCo" target="_blank">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/github.png">
+                    LucidStudiosCo
+                  </a>
+                </div>
+              </div>
+              <div class="copyright">
+                <p>&copy; 2013 - Lucid Studios, LLC</p>
+              </div>
+            </div>
+          </div>
 
-  </div> <!-- End Content -->
+        </div> <!-- End Content -->
 
   <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/libs/bootstrap.js"></script>
   <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/app.js"></script>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo('name'); ?></title>
+    <title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo( 'name' ); ?></title>
     <meta name="description" content="Developing High End Web and Mobile Applications">
 
     <!-- html5.js -->
@@ -16,8 +16,8 @@
       <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
     <![endif]-->
 
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/assets/img/favicon.png">
-    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    <link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/assets/img/favicon.png">
+    <link href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="stylesheet">
 
     <?php wp_enqueue_script('jquery'); ?>
 
@@ -25,21 +25,18 @@
   </head>
   <body>
 
-  <div class="header">
-    <ul class="hidden-xs">
-      <?php wp_nav_menu( array( 'menu' => 'Menu 1', 'container' => 'False', 'items_wrap' => '%3$s' ) ); ?>
-      <li class="logo"><a href="<?php bloginfo( 'url' ); ?>">
-        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/logo.png">
-      </a></li>
-        <?php wp_nav_menu( array( 'menu' => 'Menu 2', 'container' => 'False', 'items_wrap' => '%3$s' ) ); ?>
-    </ul>
-    <div class="visible-xs">
-      <img class="title" src="<?php bloginfo( 'template_directory' ); ?>/assets/img/title-white.png">
-      <ul>
-        <?php wp_nav_menu( array( 'menu' => 'Menu 1', 'container' => 'False', 'items_wrap' => '%3$s' ) ); ?>
-        <?php wp_nav_menu( array( 'menu' => 'Menu 2', 'container' => 'False', 'items_wrap' => '%3$s' ) ); ?>
-      </ul>
+    <div class="menu">
+      <div class="navigation">
+        <a href="<?php echo home_url(); ?>">
+          <img class="title" src="<?php bloginfo( 'template_directory' ); ?>/assets/img/title-white.png">
+        </a>
+        <ul>
+          <?php if ( has_nav_menu( 'main-menu' ) ) { ?>
+          <?php wp_nav_menu( array( 'theme-location' => 'main-menu', 'container' => 'False',
+            'items_wrap' => '%3$s' ) ); ?>
+          <?php } ?>
+        </ul>
+      </div>
     </div>
-  </div>
 
-  <div class="content">
+    <div class="content">
