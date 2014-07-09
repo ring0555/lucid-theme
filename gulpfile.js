5cee5e205eb9d0d88f694374e887e6abce9851e1
Nathan Hefner <hefner.nathan@gmail.com>
2014-07-09 09:33:12 -0400

    Ionicons gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,9 @@ var uglify = require('gulp-uglify');
 var paths = {
   scripts: 'assets/js/**/*',
   styles: 'assets/scss/**/*',
-  images: 'assets/img/**/*'
+  images: 'assets/img/**/*',
+  ionicons: 'assets/bower_components/ionicons/fonts/*',
+  ioniconsCss: 'assets/bower_components/ionicons/css/ionicons.min.css'
 };
 
 gulp.task('styles', function () {
@@ -19,14 +21,12 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('dist/css'));
 });
 
-/*
 gulp.task('vendor-styles', function() {
-  gulp.src()
+  gulp.src(paths.ioniconsCss)
     .pipe(sass())
     .pipe(concat('vendor.css'))
     .pipe(gulp.dest('dist/css'));
 });
-*/
 
 gulp.task('scripts', function() {
   // Minify and copy all JavaScript
@@ -34,6 +34,11 @@ gulp.task('scripts', function() {
     .pipe(uglify())
     .pipe(concat('main.min.js'))
     .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('fonts', function() {
+ return gulp.src(paths.ionicons)
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('images', function() {
@@ -44,8 +49,10 @@ gulp.task('images', function() {
 // Rerun the task when a file changes
 gulp.task('watch', function() {
   gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.ioniconsCss, ['vendor-styles']);
   gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.ionicons, ['fonts']);
   gulp.watch(paths.images, ['images']);
 });
 
-gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
+gulp.task('default', ['styles', 'vendor-styles', 'scripts', 'fonts', 'images', 'watch']);
