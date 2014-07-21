26015a09508da6869ff2ef3cf53f9b4c5a25815f
Nathan Hefner <hefner.nathan@gmail.com>
2014-07-21 14:25:12 -0400

    Updates to single post view

--- single.php
+++ single.php
@@ -1,23 +1,15 @@
 <?php get_header(); ?>
-
-  <div class="single-post">
-
+  <div id="single-post">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
+    <div class="page-heading">
+      <h1><?php the_title(); ?></h1>
+      <h4>By: <?php the_author(); ?></h4>
+      <h4><?php the_time('F jS, Y'); ?></h4>
+    </div>
     <div class="post">
-      <div class="top">
-        <h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-        <h5 class="date"><?php the_time('F jS, Y'); ?></h5>
-      </div>
-      <h5 class="author">By: <?php the_author(); ?></h5>
       <div class="post-content"><?php the_content(); ?></div>
-
     <?php endwhile; else: ?>
-
       <?php _e('Sorry, this page does not exist'); ?>
-
     <?php endif; ?>
-
   </div>
-
 <?php get_footer(); ?>
