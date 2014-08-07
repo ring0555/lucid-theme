128246f7d0a9aa8e76824bc584e18544403c1aa9
Nathan Hefner <hefner.nathan@gmail.com>
2014-08-07 09:50:44 -0400

    Sorting for journal

--- home.php
+++ home.php
@@ -2,7 +2,15 @@
 
 <div class="page-heading">
   <h1>Journal</h1>
-  <h4>Read what we're writing about.</h4>
+  <ul class="sort">
+    <li data-sort='all'>All</li>
+    <?php
+      $post_categories = get_categories( array( 'hide_empty' => '0' ) );
+      foreach ( $post_categories as $post_category ) {
+        echo '<li data-sort="'.$post_category->slug.'">'.$post_category->name.'</li>';
+      }
+    ?>
+  </ul>
 </div>
 
 <div id="journal">
@@ -12,7 +20,23 @@
   <?php if (have_posts()) : ?>
     <?php while ( have_posts() ) : the_post(); ?>
 
-    <div class="post">
+    <?php
+      $categories = wp_get_post_categories( $post->ID );
+      $categories_len = count( $categories );
+      $categories_str = "[";
+      $index = 0;
+      foreach ( $categories as $key=>$category ) {
+        if ( $index == $categories_len - 1 ) {
+          $category_comma = '"'.$category->slug.'", "all"]';
+        } else {
+          $category_comma = '"'.$category->slug.'", ';
+        }
+        $categories_str .= $category_comma;
+        $index++;
+      }
+    ?>
+
+    <div class="post sortable" data-categories='<?php echo $categories_str; ?>'>
       <div class="image">
         <?php if ( has_post_thumbnail() ) { ?>
         <a href="<?php the_permalink(); ?>">
