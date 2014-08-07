eb6248f172f0bd8bccc39ebf887ca15f44c82970
Nathan Hefner <hefner.nathan@gmail.com>
2014-08-07 09:33:29 -0400

    Move sorting function to JS file

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,16 +6,24 @@ jQuery(document).ready(function($) {
     $('.menu-icon').click(function() {
       if ($('ul', '.navigation').hasClass('open')) {
         $('ul', '.navigation').removeClass('open');
-        $('ul', '.navigation').slideUp();
+        $('ul', '.navigation').hide();
       } else {
         $('ul', '.navigation').addClass('open');
-        $('ul', '.navigation').slideDown();
+        $('ul', '.navigation').show();
       }
     });
 
-    // Mobile Nav
-    $('.menu-icon').click(function() {
-      $('ul', '.navigation').show();
+    // Sort Function
+    $('ul.sort li').click(function() {
+      var sort = $(this).data('sort');
+      $('.sortable').each(function() {
+        var categories = $(this).data('categories');
+        if ( $.inArray(sort, categories) === -1 ) {
+          $(this).delay(400).fadeOut(600);
+        } else {
+          $(this).delay(400).fadeIn(600);
+        }
+      });
     });
 
 });
