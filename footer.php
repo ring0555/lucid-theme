d37f7c0f841774401044067da0721c15c5a54b6c
Nathan Hefner <hefner.nathan@gmail.com>
2014-08-07 09:32:01 -0400

    Remove already included script

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 
         </div> <!-- End Content -->
 
-  <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/app.js"></script>
   <?php wp_footer(); ?>
   </body>
 </html>
